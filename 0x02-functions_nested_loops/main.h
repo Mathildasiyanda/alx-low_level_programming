be77610708463d4ebafa36f677ef5b99685a6e82
mathildasiyanda <moganemathilda@gmail.com>
2023-03-21 07:00:01 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,13 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char);
+/**
+ * File: main.h
+ * Description: contains all functions prototype used in this project
+ */
+
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,7 +19,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
 
-
-#endif
+#endif /* MAIN_H */
