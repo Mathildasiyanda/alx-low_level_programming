f4f8204380320d307d3824a650050080fd852984
mathildasiyanda <moganemathilda@gmail.com>
2023-04-28 12:55:34 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * list_len - Counts the number of nodes in a linked list.
+ *
+ * @h: A pointer to the head node of the linked list.
+ *
+ * Return: The number of nodes in the linked list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
